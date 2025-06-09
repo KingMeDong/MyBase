bc6827dfd1dfccc1c2ea6f46534738b0fcd47e58
Lasse Hinderer <lassehinderer@gmx.de>
2025-06-09 17:46:51 +0200

    Login

--- MyBase/Pages/Account/Login.cshtml.cs
+++ MyBase/Pages/Account/Login.cshtml.cs
@@ -1,12 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyBase.Data;
+using MyBase.Models;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
 
-namespace MyBase.Pages.Account
-{
-    public class LoginModel : PageModel
-    {
-        public void OnGet()
-        {
+namespace MyBase.Pages.Account {
+    public class LoginModel : PageModel {
+        private readonly AppDbContext _db;
+
+        public LoginModel(AppDbContext db) {
+            _db = db;
+        }
+
+        [BindProperty]
+        public string Username { get; set; }
+
+        [BindProperty]
+        public string Password { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public void OnGet() {
+        }
+
+        public IActionResult OnPost() {
+            var user = _db.Users.FirstOrDefault(u => u.Username == Username);
+
+            if (user != null && user.PasswordHash == Password) // später Hash vergleichen!
+            {
+                // Session setzen
+                HttpContext.Session.SetString("Username", user.Username);
+                HttpContext.Session.SetString("DisplayName", user.DisplayName ?? user.Username);
+
+                return RedirectToPage("/Index"); // später Dashboard
+            } else {
+                ErrorMessage = "Ungültiger Benutzername oder Passwort.";
+                return Page();
+            }
         }
     }
 }
--- MyBase/Program.cs
+++ MyBase/Program.cs
@@ -10,11 +10,19 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
+builder.Services.AddSession();
+
+
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseSession();
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
