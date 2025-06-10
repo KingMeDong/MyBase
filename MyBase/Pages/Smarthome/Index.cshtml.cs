4389ca787d2c5a0d4dde83308700b7a06c208e04
Lasse Hinderer <lassehinderer@gmx.de>
2025-06-10 15:14:58 +0200

    Smarthome

---
+++ MyBase/Pages/Smarthome/Index.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyBase.Pages.SmartHome {
+    public class IndexModel : PageModel {
+        public void OnGet() {
+            // später: Sensor-Daten laden, Geräte-Status abrufen etc.
+        }
+    }
+}
